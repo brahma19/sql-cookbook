e1e43b4c2f166df42f0858be6e31caed925e6fef
Brahmanand Singh <59507487+brahma19@users.noreply.github.com>
2022-06-19 22:57:32 +0530

    Update bus_passenger.sql

--- sql/bus_passenger.sql
+++ sql/bus_passenger.sql
@@ -18,11 +18,27 @@
 -- insert into passenger values(12,5);
 -- insert into passenger values(13,6);
 -- insert into passenger values(14,7);
+-- insert into passenger values(15,7);
+-- insert into passenger values(16,3);
+-- insert into passenger values(17,2);
+-- insert into passenger values(18,1);
 
-select * from bus,passenger p
-where bus.arrival_time>=p.arrival_time;
+select bus_id,
+sum(
+  case when patime-nvl(last_at,0) = 0 and p_id=last_pid
+  then null
+  else 1 end
+  ) cnt from 
+  (
+select b.id bus_id,p.p_id,p.arrival_time patime, 
+lag(p.arrival_time,1) over( order by p.arrival_time) last_at,
+lag(p.p_id,1) over( order by p.arrival_time) last_pid 
+from bus b,passenger p
+where p.arrival_time<=b.arrival_Time
+) a group by bus_id;
 
 
 -- select * from bus;
 
--- select * from passenger;
+select * from passenger;
+delete from passenger where p_id >=15;
